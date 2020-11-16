b0817ab622d314300a20dabf02fbd0ffa2cf65d2
Aryaman <54187786+wickedbaba@users.noreply.github.com>
2020-11-16 13:42:43 +0530

    Add files via upload

---
+++ tREE.java
@@ -0,0 +1,17 @@
+
+package Assignment3;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class tREE extends Application{
+
+    public static void main(String args[]) {
+       
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
